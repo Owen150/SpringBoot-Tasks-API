e6e44aebffcfd1d9d2eec1fffcb725d73aaabe04
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-02-06 14:54:35 +0300

    First Commit

---
+++ src/main/java/com/tasks/springboottasksapp/SpringbootTasksAppApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.springboottasksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTasksAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTasksAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasks/springboottasksapp/SpringbootTasksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.springboottasksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTasksAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
